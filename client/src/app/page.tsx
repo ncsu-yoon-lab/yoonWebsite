e6f70e1e325a06c4454a0b9e3e82b4b779491ae0
PratikThapa24 <prajwaljungthapa@gmail.com>
2025-05-01 13:37:47 -0400

    Finished working on NavBar

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,20 @@
+// app/page.tsx
 import Image from "next/image";
-import styles from "./page.module.css";
+import NavBar from "@/components/NavBar/NavBar";
 
 export default function Home() {
-  return (<>
-    <h1 className="text-5xl text-center"> Yoon's Lab</h1>
-    <p className="text-xl text-center">Where Creativity Meets Passion</p>
-  </>);
+  return (
+    <div className="min-h-screen bg-gradient-to-b font-montserrat tracking-wide">
+      <NavBar />
+      {/* Spacer to account for the fixed navbar */}
+      <div className="h-10"></div>
+      {/* Horizontal line divider */}
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="w-full h-0.5 bg-gray-200 dark:bg-gray-700"></div>
+      </div>
+      <main className="tracking-wider">
+
+      </main>
+    </div>
+  );
 }
